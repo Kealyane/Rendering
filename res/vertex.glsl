53ff0a9e0b6870bfbb0babd94e0520a3b14f9381
Laurene <laurene.broquet@gmail.com>
2024-04-19 09:41:58 +0200

    move rectangle

--- res/vertex.glsl
+++ res/vertex.glsl
@@ -4,5 +4,6 @@ layout(location = 0) in vec2 in_position;
 
 void main()
 {
-    gl_Position = vec4(in_position, 0., 1.);
+    vec2 newPos = in_position + vec2(0.4, 0.4);
+    gl_Position = vec4(newPos, 0., 1.);
 }
