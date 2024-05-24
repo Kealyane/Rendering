56233f459181d7d24b53708094485014d324b948
Laurene <laurene.broquet@gmail.com>
2024-05-24 18:49:03 +0200

    Filter NearestNeighbour

--- src/main.cpp
+++ src/main.cpp
@@ -58,10 +58,10 @@ int main()
     .vertex_buffers = {{
         .layout = {gl::VertexAttribute::Position2D{0},gl::VertexAttribute::UV{1}},
         .data   = {
-            -0.5f, -0.5f, -1.f, -1.f, // Position2D du 1er sommet - 0
-            +0.5f, -0.5f, 2.f, -1.f, // Position2D du 2ème sommet - 1
-            +0.5f, +0.5f, 2.f, 2.f, // Position2D du 3ème sommet - 2
-            -0.5f, +0.5f, -1.f, 2.f  // Position2D du 4ème sommet - 3
+            -0.5f, -0.5f, 0.8f, 0.8f, // Position2D du 1er sommet - 0
+            +0.5f, -0.5f, 0.9f, 0.8f, // Position2D du 2ème sommet - 1
+            +0.5f, +0.5f, 0.9f, 0.9f, // Position2D du 3ème sommet - 2
+            -0.5f, +0.5f, 0.8f, 0.9f  // Position2D du 4ème sommet - 3
         },
     }},
     .index_buffer   = {
@@ -101,11 +101,10 @@ int main()
         .texture_format = gl::InternalFormat::RGBA8, // Format dans lequel la texture sera stockée. On pourrait par exemple utiliser RGBA16 si on voulait 16 bits par canal de couleur au lieu de 8. (Mais ça ne sert à rien dans notre cas car notre fichier ne contient que 8 bits par canal, donc on ne gagnerait pas de précision). On pourrait aussi stocker en RGB8 si on ne voulait pas de canal alpha. On utilise aussi parfois des textures avec un seul canal (R8) pour des usages spécifiques.
     },
     gl::TextureOptions{
-        .minification_filter  = gl::Filter::Linear, // Comment on va moyenner les pixels quand on voit l'image de loin ?
-        .magnification_filter = gl::Filter::Linear, // Comment on va interpoler entre les pixels quand on zoom dans l'image ?
-        .wrap_x               = gl::Wrap::ClampToBorder,//ClampToEdge,//MirroredRepeat,//Repeat,   // Quelle couleur va-t-on lire si jamais on essaye de lire en dehors de la texture ?
-        .wrap_y               = gl::Wrap::ClampToBorder,//ClampToEdge,//MirroredRepeat,//Repeat,   // Idem, mais sur l'axe Y. En général on met le même wrap mode sur les deux axes.
-        .border_color         = glm::vec4(1.f),
+        .minification_filter  = gl::Filter::NearestNeighbour,//Linear, // Comment on va moyenner les pixels quand on voit l'image de loin ?
+        .magnification_filter = gl::Filter::NearestNeighbour,//Linear, // Comment on va interpoler entre les pixels quand on zoom dans l'image ?
+        .wrap_x               = gl::Wrap::MirroredRepeat,//Repeat,   // Quelle couleur va-t-on lire si jamais on essaye de lire en dehors de la texture ?
+        .wrap_y               = gl::Wrap::MirroredRepeat,//Repeat,   // Idem, mais sur l'axe Y. En général on met le même wrap mode sur les deux axes.
     }
     };
 
