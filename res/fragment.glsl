2bad83a4a70c5798437d72445665521f5478849a
Laurene <laurene.broquet@gmail.com>
2024-07-05 09:15:36 +0200

    ambiant light

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -7,11 +7,14 @@ in vec3 out_normal;
 uniform sampler2D my_texture;
 uniform vec3 light_direction;
 
+const float ambiant_light = 0.3;
+
 void main()
 {
     vec4 texture_color = texture(my_texture, out_uv);
     vec3 normal = normalize(out_normal);
     float light = max(dot(normal, light_direction), 0.0);
+    light = light + ambiant_light;
     out_color = vec4(texture_color.rgb * light, 1.0);
     
     //out_color = texture_color;
